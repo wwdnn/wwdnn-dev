f7a90b3d4826a101118a201e8640149c6ace6e21
wwdnn <wildannugraha330@gmail.com>
2026-07-16 07:11:41 +0700

    [FIX] bug indexing z component

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,20 +2,26 @@
 export const PROJECTS = [
     {
         id: 1,
-        title: "NTI Rain Calc",
+        title: "Dashboard LMS",
         tags: ["Odoo", "Python", "QWeb", "PostgreSQL"],
         accent: "#3b2ff5",
     },
     {
         id: 2,
-        title: "NTI Siphonic Calc",
+        title: "Siphonic Calc",
         tags: ["Odoo 18", "Python", "OWL", "SciPy", "QWeb", "PostgreSQL"],
         accent: "#3b2ff5",
     },
     {
         id: 3,
-        title: "NTI Rain Master",
+        title: "Organization Chart",
         tags: ["Odoo", "Python", "OWL", "XML"],
         accent: "#3b2ff5",
     },
+    // {
+    //     id: 4,
+    //     title: "Dashboard HR",
+    //     tags: ["Odoo", "Python", "OWL", "XML"],
+    //     accent: "#3b2ff5",
+    // },
 ];
